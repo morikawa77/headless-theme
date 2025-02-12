e2ada284f9a2831c6883eeafe480210ef2627592
Reginaldo Morikawa <morikawa77@gmail.com>
2025-02-12 16:36:16 -0300

    Fix: Wordpress minimal reqs

--- index.php
+++ index.php
@@ -24,66 +24,70 @@ function redirecionar($url)
 }
 
 redirecionar(get_option('url_to_redirect'));
+?>
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+  <?php wp_body_open(); ?>
 
-<header>
-    <?php 
+  <header>
+    <?php
     if (has_custom_logo()) {
-        the_custom_logo();
+      the_custom_logo();
     } else {
-        echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>';
+      echo '<a href="' . home_url() . '">' . get_bloginfo('name') . '</a>';
     }
-    
+
     wp_nav_menu(array(
-        'theme_location' => 'primary',
-        'container_class' => 'main-menu'
+      'theme_location' => 'primary',
+      'container_class' => 'main-menu'
     )); ?>
-</header>
+  </header>
 
-<main>
+  <main>
     <?php while (have_posts()) : the_post(); ?>
-        <article <?php post_class(); ?>>
-            <h2><?php the_title(); ?></h2>
-            <?php the_content(); ?>
-            <?php 
-                wp_link_pages(array(
-                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'headless-by-wolfpartners'),
-                    'after'  => '</div>',
-                ));
-            ?>
-            <?php if (has_tag()) : ?>
-                <div class="tags"><?php the_tags('', ' '); ?></div>
-            <?php endif; ?>
-        </article>
-        
-        <?php 
-        if (comments_open() || get_comments_number()) {
-            comments_template();
-        }
+      <article <?php post_class(); ?>>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+        <?php
+        wp_link_pages(array(
+          'before' => '<div class="page-links">' . esc_html__('Pages:', 'headless-by-wolfpartners'),
+          'after'  => '</div>',
+        ));
         ?>
-        
+        <?php if (has_tag()) : ?>
+          <div class="tags"><?php the_tags('', ' '); ?></div>
+        <?php endif; ?>
+      </article>
+
+      <?php
+      if (comments_open() || get_comments_number()) {
+        comments_template();
+      }
+      ?>
+
     <?php endwhile; ?>
-</main>
+  </main>
 
-<?php get_sidebar(); ?>
+  <?php get_sidebar(); ?>
 
-<footer>
-    <?php 
+  <footer>
+    <?php
     wp_nav_menu(array(
-        'theme_location' => 'footer',
-        'container_class' => 'footer-menu'
-    )); 
+      'theme_location' => 'footer',
+      'container_class' => 'footer-menu'
+    ));
     ?>
     <?php wp_footer(); ?>
-</footer>
+  </footer>
 </body>
+
 </html>
